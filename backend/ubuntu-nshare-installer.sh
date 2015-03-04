b381e945349b55577125cd569522f9f4cc31cc4b
admin12 <terkelsorensen@gmail.com>
2015-03-04 09:49:18 +0100

    update

--- backend/ubuntu-nshare-installer.sh
+++ backend/ubuntu-nshare-installer.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
+apt-get install wget
 apt-get install tar
-apt-get install git
 apt-get install nodejs
 apt-get install npm
 apt-get install chromium-browser
 
-wget https://github.com/voidcode/nshare-chrome-extension/archive/master.zip | tar xz
+curl -L https://github.com/voidcode/nshare-chrome-extension/archive/master.zip | tar xz
 cd nshare-chrome-extension/nshare-chrome-extension/
-npm install
+npm installls
 npm install -g forever
 cd backend/
 forever start nshare-demon.js
